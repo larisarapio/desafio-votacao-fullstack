aa57b4448bc48e0534dce20d240c76df1618e3b3
devlarisarapio <larissa.sarapio@gmail.com>
2024-12-08 11:32:58 -0300

    feat: implementando estrutura base projeto

---
+++ votacao-api/src/test/java/com/sarapio/votacao_api/VotacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarapio.votacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
