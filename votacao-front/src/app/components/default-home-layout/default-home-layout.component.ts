ab0e58e1809e17ecde81b799ab1e6e0a4d4a065a
devlarisarapio <larissa.sarapio@gmail.com>
2024-12-09 01:37:23 -0300

    feat: criando estrutura inicial

---
+++ votacao-front/src/app/components/default-home-layout/default-home-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-home-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-home-layout.component.html',
+  styleUrl: './default-home-layout.component.scss'
+})
+export class DefaultHomeLayoutComponent {
+
+}
